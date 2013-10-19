7a5b4b337fe09b8b0924634262a661b2dd22001d
Earthmark (Laptop) <earthmark.miller@gmail.com>
2013-10-19 16:41:55 -0700

    Removed SharpDX changes.

--- Struct-of-Structs/Program.cs
+++ Struct-of-Structs/Program.cs
@@ -1,77 +1,13 @@
 ﻿using System;
 using System.IO;
-using System.Windows.Forms;
-using SharpDX;
-using SharpDX.Windows;
-using Struct_of_Structs.Control;
-using Struct_of_Structs.Visual;
 
 namespace Struct_of_Structs
 {
-	class Program : Cleanup
+	class Program
 	{
-		private readonly RenderForm form;
-		private readonly Graphics graphics;
-		private readonly Input input;
-		public TimerTick Timer { get; private set; }
-		private bool closed;
-
-		public bool Closed
-		{
-			get { return closed; }
-			set
-			{
-				closed = value;
-				if (!closed && value)
-					form.Close();
-			}
-		}
-
 		static void Main(string[] args)
 		{
-			var newPath = Path.Combine(Environment.CurrentDirectory, Environment.Is64BitProcess ? "x64" : "x86") + ";" + Environment.GetEnvironmentVariable("PATH");
-			Environment.SetEnvironmentVariable("PATH", newPath);
 
-			using(var program = new Program())
-			{
-				program.Run();
-			}
-		}
-
-		private Program()
-		{
-			form = new RenderForm("Struct-of-Structs");
-			graphics = new Graphics(800, 600, false, form.Handle);
-			input = new Input(form);
-			Timer = new TimerTick();
-
-			OnCleanup += graphics.Dispose;
-			OnCleanup += input.Dispose;
-			OnCleanup += form.Dispose;
-		}
-
-		private void Run()
-		{
-			RenderLoop.Run(form, Loop);
-		}
-
-		private void Loop()
-		{
-			if (Closed)
-				return;
-
-			Timer.Tick();
-
-			if (input[Keys.Escape])
-			{
-				form.Close();
-			}
-			else
-			{
-				graphics.Frame();
-			}
 		}
 	}
-
-
 }
--- Struct-of-Structs/Visual/Vertexes/IVertex.cs
+++
@@ -1,9 +0,0 @@
-﻿using SharpDX.Direct3D11;
-
-namespace Struct_of_Structs.Visual.Vertexes
-{
-	public interface IVertex
-	{
-		InputElement[] Layout();
-	}
-}
